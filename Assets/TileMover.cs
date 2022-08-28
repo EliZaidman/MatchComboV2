d76fb8ef8ae110482b71823be6df19c27a3940ff
Eli Zaidman <Rxelik5@gmail.com>
2022-08-28 10:30:04 +0300

    Optimized New MoveTile Method

--- Assets/TileMover.cs
+++ Assets/TileMover.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,27 +10,41 @@ public class TileMover : MonoBehaviour
     int FindSlot;
     Magazine mag;
     Tile tile;
+    bool CanSort;
     private void Start()
     {
+        EventManager.Instance.CorutineStopper += StopAnimCor;
+        EventManager.Instance.CorutineStarter += StartAnim;
         tile = GetComponent<Tile>();
         sort = TileSorter.Instance;
         mag = Magazine.Instance;
     }
     private void Update()
     {
-        if (mag.SortedMagazine.Contains(this.tile))
+        if (mag.SortedMagazine.Contains(this.tile) && CanSort)
         {
             FindSlot = mag.SortedMagazine.IndexOf(this.tile);
+            if (transform.position != mag.MagazineSlots[FindSlot].transform.position)
+            {
             StartCoroutine(Sort());
+            }
         }
-
-
     }
     IEnumerator Sort()
     {
+        print("Sorting" + this);
         yield return new WaitForEndOfFrame();
         float t = 0;
         t += Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, mag.MagazineSlots[FindSlot].position, t / sort.TileMoveSpeed);
+    }
+
+    private void StartAnim(object sender, EventArgs e)
+    {
+        CanSort = true;
+    }
+    private void StopAnimCor(object sender, EventArgs e)
+    {
+        CanSort = false;
     }
 }
--- Assets/TileSorter.cs
+++ Assets/TileSorter.cs
@@ -20,51 +20,43 @@ public class TileSorter : MonoBehaviour
             Instance = this;
         }
     }
-    Magazine _mag;
-    private void Start()
-    {
-        _mag = Magazine.Instance;
-        EventManager.Instance.CorutineStopper += StopAnimCor;
-        EventManager.Instance.CorutineStarter += StartAnim;
-    }
+    //Magazine _mag;
+    //private void Start()
+    //{
+    //    _mag = Magazine.Instance;
 
-    public bool StopAnim = false;
-    private void LateUpdate()
-    {
-        //Change The Speed Depending if you won or not
-        //if (!StopAnim)
-            //StartCoroutine(SortMagazineCorutine(0.5f));
-        //else
-        //    SortMagazineCorutine(0.01f);
+    //}
+
+    //public bool StopAnim = false;
+    //private void LateUpdate()
+    //{
+    //    //Change The Speed Depending if you won or not
+    //    //if (!StopAnim)
+    //        //StartCoroutine(SortMagazineCorutine(0.5f));
+    //    //else
+    //    //    SortMagazineCorutine(0.01f);
 
 
-    }
-    private void StartAnim(object sender, EventArgs e)
-    {
-        StopAnim = false;
-    }
-    private void StopAnimCor(object sender, EventArgs e)
-    {
-        StopAnim = true;
-    }
+    //}
 
-    public IEnumerator SortMagazineCorutine(float duration)
-    {
-        int count = 0;
+
+    //public IEnumerator SortMagazineCorutine(float duration)
+    //{
+    //    int count = 0;
        
-        //Takes the SortedMagazine and automaticly change its position to the magSlot
-        //Yes its EVERY Second trying to fix its position
-        //ONE DAY IT WILL BE BETTER I SWEAR
-        //I WILL LEARN HOW TO MAKE IT BETTER
-        //SOMETIMES TO WIN A WAR YOU MUST LOOSE A FIGHT!
-        float t = 0;
-        yield return new WaitUntil(() => _mag.SortedMagazine[count].transform.position == _mag.SortedMagazine[count].transform.position);
-        for (int i = 0; i < _mag.SortedMagazine.Count; i++)
-        {
-            t += Time.deltaTime;
-            _mag.SortedMagazine[i].transform.position = Vector2.MoveTowards(_mag.SortedMagazine[i].transform.position, _mag.MagazineSlots[i].position, t / duration);
-            count++;
-        }
-        count = 0;
-    }
+    //    //Takes the SortedMagazine and automaticly change its position to the magSlot
+    //    //Yes its EVERY Second trying to fix its position
+    //    //ONE DAY IT WILL BE BETTER I SWEAR
+    //    //I WILL LEARN HOW TO MAKE IT BETTER
+    //    //SOMETIMES TO WIN A WAR YOU MUST LOOSE A FIGHT!
+    //    float t = 0;
+    //    yield return new WaitUntil(() => _mag.SortedMagazine[count].transform.position == _mag.SortedMagazine[count].transform.position);
+    //    for (int i = 0; i < _mag.SortedMagazine.Count; i++)
+    //    {
+    //        t += Time.deltaTime;
+    //        _mag.SortedMagazine[i].transform.position = Vector2.MoveTowards(_mag.SortedMagazine[i].transform.position, _mag.MagazineSlots[i].position, t / duration);
+    //        count++;
+    //    }
+    //    count = 0;
+    //}
 }
